c7b96ff27a708e76a32803461fe9bd049fd78605
samwasted <samarpancodes@gmail.com>
2025-07-09 00:20:05 +0530

    fix unused imports for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 
--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,9 +1,9 @@
 'use client'
 import React, { useState } from 'react'
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+// import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 
 function About() {
-  const [loading, setLoading] = useState(true)
+//   const [loading, setLoading] = useState(true)
 
   return (
     <div className="container m-20 text-center w-full text-white">
